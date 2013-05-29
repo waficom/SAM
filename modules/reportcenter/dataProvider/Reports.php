068fe7d4341c8bfde93006c705836adac76f4beb
dharma <dharma@untusoft.com>
2013-05-29 09:19:30 +0700

    update bisa parameter tanggal

--- modules/reportcenter/dataProvider/Reports.php
+++ modules/reportcenter/dataProvider/Reports.php
@@ -33,8 +33,110 @@ class Reports
         return;
     }
 
+    public function DateConvert($date) {
+
+        # Exception
+        if (!is_null($date))
+        {
+            $date = new DateTime($date, new DateTimeZone($_SESSION['site']['timezone']));
+            $strdate = $date->format('m/d/Y H:i:s');
+
+            # Separate Date from Time
+            $strdate = explode(" ", $strdate);
+
+            $date = $strdate[0];
+
+        }
+
+        return $date;
+
+    } # End Function
+
+    public function convertValue($value, $className)
+
+    {
+
+        // if we are a string, just use the normal conversion
+
+        // methods from the java extension...
+
+        try
+
+        {
+
+            if ($className == 'java.lang.String')
+
+            {
+
+                $temp = new Java('java.lang.String', $value);
+
+                return $temp;
+
+            }
+
+            else if ($className == 'java.util.Date')
+
+            {
+
+                $temp = new Java($className, $value);
+
+
+                return $temp;
+
+            }
+
+            else if ($className == 'java.lang.Boolean' ||
+
+                $className == 'java.lang.Integer' ||
+
+                $className == 'java.lang.Long' ||
+
+                $className == 'java.lang.Short' ||
+
+                $className == 'java.lang.Double' ||
+
+                $className == 'java.math.BigDecimal')
+
+            {
+
+                $temp = new Java($className, $value);
+
+                return $temp;
+
+            }
+
+            else if ($className == 'java.sql.Timestamp' ||
+
+                $className == 'java.sql.Time')
+
+            {
+
+                $temp = new Java($className);
+
+                $javaObject = $temp->valueOf($value);
+
+                return $javaObject;
+
+            }
+
+        }
+
+        catch (Exception $err)
+
+        {
+
+            echo (  'unable to convert value, ' . $value .
+
+                ' could not be converted to ' . $className);
+
+            return false;
+
+        }
+    }
+
     public function report_parse_post_parameters(stdClass $parameters)
     {
+        error_reporting(-1);
         $data = get_object_vars($parameters);
 
         # Automatically extract report parameters (data types converted in report).
@@ -50,7 +152,7 @@ class Reports
                     $value = intval( $value );
                     $length = strlen( 'report_int_' );
 
-                    $value = convertValue( $value, 'java.lang.Integer' );
+                    $value = $this->convertValue( $value, 'java.lang.Integer' );
                     $params->put( substr( $name, $length ), $value );
                 }
                 else if( strpos( $name, 'report_arr_' ) === 0 ) {
@@ -66,6 +168,15 @@ class Reports
 
                     # Pass values into the report (without the "report_arr_" prefix).
                     $params->put( substr( $name, $length ), $arrayList );
+                }
+                else if ( strpos($name, 'report_date_') === 0) {
+                    $length = strlen ('report_date_');
+
+                    $value = $this->DateConvert( $value );
+
+                    $value = $this->convertValue($value, 'java.util.Date');
+
+                    $params->put(substr($name,$length), $value);
                 }
                 else {
                     $params->put( substr( $name, $length ), $value );
@@ -141,13 +252,13 @@ class Reports
 
 }
 
-//
-//$r = new Reports();
-//$r->reportfile = '/var/www/sam-new/bs.jasper';
-//$stdc = new StdClass;
-//$stdc->report_staff = 'IMAM';
-//$r->report_execute($stdc);
-//print_r($r->uri);
-//print_r($r->pdffile);
-
+/*
+$r = new Reports();
+$r->reportfile = '/var/www/sam-new/modules/reportcenter/report/marketing/bs.jasper';
+$stdc = new StdClass;
+$stdc->report_date_datefrom = '01/01/2009';
+$stdc->report_date_dateto = '01/01/2010';
+$r->report_execute($stdc);
+print_r($r);
+*/
 
